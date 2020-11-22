38b8a0552caf258e4823be5da79d135300d46ff5
Nishanth <nishanthprabhakaran1998@gmail.com>
2020-11-22 23:47:06 +0530

    Added Hello in WeatherForecast

--- API/TaskManager.API/Controllers/WeatherForecastController.cs
+++ API/TaskManager.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TaskManager.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hello"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
