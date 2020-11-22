c417bf106f71720b807427286f3dd6fa89efc48e
Nishanth <nishanthprabhakaran1998@gmail.com>
2020-11-23 00:13:00 +0530

    Added Cold

--- API/TaskManager.API/Controllers/WeatherForecastController.cs
+++ API/TaskManager.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TaskManager.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Summer", "Winter", "Autumn", "Spring"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Summer", "Winter", "Autumn", "Spring","Cold"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
