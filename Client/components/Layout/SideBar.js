992c0f617a70fa4eebe2a74b23fe4be9a3334a0f
Vasanth <vasanthprabhakaran1999@gmail.com>
2021-04-13 00:57:24 +0530

    changed logo

--- Client/components/Layout/SideBar.js
+++ Client/components/Layout/SideBar.js
@@ -3,9 +3,9 @@ function SideBar() {
         <div className="sidebar">
             <img
                 className="pt-5 mx-auto"
-                src="/logo.png"
+                src="/logo.svg"
                 alt="taskManager"
-                width={200}
+                width="200"
             />
         </div>
     );
