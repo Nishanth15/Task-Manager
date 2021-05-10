941c65049391ea746fcdd012ea6b5b11599e0a8e
Vasanth Prabhakaran <55186278+Vasanth1009@users.noreply.github.com>
2021-05-10 13:15:44 +0530

    Delete Client/src/pages directory

--- Client/src/pages/inbox/index.js
+++
@@ -1,9 +0,0 @@
-const Inbox = () => {
-    return (
-        <div>
-            <h1 className="text-2xl">Inbox</h1>
-        </div>
-    );
-};
-
-export default Inbox;
--- Client/src/pages/label/index.js
+++
@@ -1,9 +0,0 @@
-const Label = () => {
-    return (
-        <div>
-            <h1 className="text-2xl">Label</h1>
-        </div>
-    );
-};
-
-export default Label;
