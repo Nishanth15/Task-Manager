844072139aa69de4344586d44d396359e5af8cc4
Vasanth <vasanthprabhakaran1999@gmail.com>
2021-05-10 13:34:14 +0530

    Created pages

---
+++ Client/src/pages/Calender/index.js
@@ -0,0 +1,9 @@
+const Calender = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Calender</h2>
+        </div>
+    );
+};
+
+export default Calender;
---
+++ Client/src/pages/Inbox/index.js
@@ -0,0 +1,9 @@
+const Inbox = () => {
+    return (
+        <div>
+            <h1 className="text-2xl">Inbox</h1>
+        </div>
+    );
+};
+
+export default Inbox;
