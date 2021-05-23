02124e0ad43cb755eb9140081a6138a1775567f1
Nishanth <nishanthprabhakaran1998@gmail.com>
2021-05-23 17:25:40 +0530

    Udpated Startup.cs with Authorize attributes

--- API/TaskManager.API/Startup.cs
+++ API/TaskManager.API/Startup.cs
@@ -13,6 +13,9 @@ using TaskManager.DataManager.Interfaces;
 using TaskManager.API.Helpers;
 using TaskManager.API.DTOs;
 using TaskManager.API.Common.Helpers;
+using System.Collections.Generic;
+using System.IO;
+using System;
 
 namespace TaskManager.API
 {
@@ -50,6 +53,36 @@ namespace TaskManager.API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TaskManager.API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+          }
+        });
             });
 
             services.AddDbContext<UserDbContext>(options =>
